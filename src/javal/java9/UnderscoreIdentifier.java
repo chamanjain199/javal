d83ff321e4012c045dc71d57e00dfc2808c8b550
Chaman jain <chamanjain199@gmail.com>
2022-01-11 00:00:43 +0530

    Create UnderscoreIdentifier.java

---
+++ src/javal/java9/UnderscoreIdentifier.java
@@ -0,0 +1,14 @@
+package javal.java9;
+
+public class UnderscoreIdentifier {
+
+	public static void main(String[] args) {
+
+		//int _=10; //'_' is a keyword from source level 9 onwards, cannot be used as identifier
+
+		int __=10;  
+		System.out.println(__);
+
+	}
+
+}
